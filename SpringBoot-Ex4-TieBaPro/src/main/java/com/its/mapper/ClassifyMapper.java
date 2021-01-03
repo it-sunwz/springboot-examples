87e1fa5ee7afe4ac5446e458696d37780294772a
one meter sunshine <76551077+it-sunwz@users.noreply.github.com>
2021-01-03 16:34:30 +0800

    删除重新上传

--- SpringBoot-Ex4-TieBaPro/src/main/java/com/its/mapper/ClassifyMapper.java
+++
@@ -1,8 +0,0 @@
-package com.its.mapper;
-
-import com.its.po.Classify;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ClassifyMapper extends tk.mybatis.mapper.common.Mapper<Classify> {
-}
--- SpringBoot-Ex4-TieBaPro/src/main/java/com/its/service/ClassifyService.java
+++
@@ -1,9 +0,0 @@
-package com.its.service;
-
-import com.its.po.Classify;
-
-import java.util.List;
-
-public interface ClassifyService {
-    public List<Classify> findClassifyList();
-}
